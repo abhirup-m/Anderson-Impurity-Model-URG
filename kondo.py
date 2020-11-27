64286cf33ff38bc64208b267623f7bf15587ceaa
SearyBlue <mukherjeeabhirup44@gmail.com>
2020-11-27 10:54:56 +0530

    modified program to break at fixed point

--- kondo.py
+++ kondo.py
@@ -6,11 +6,20 @@ J = 0.01
 k = 2.83
 b = 0.999999999999
 K = []
+flag = False
+sgn = 1 - (J/(4*0.7075*k))**(2)
 for c in np.arange(100,10,-1):
-    K.append(J/k)
-    delta_J = 3*J**2/(1 - (J/(4*0.7075*k))**(2))
-    k *= b
-    J += delta_J
+    if sgn*(1 - (J/(4*0.7075*k))**(2)) <=0:
+        flag = True
+    else:
+        sgn = 1 - (J/(4*0.7075*k))**(2)
+    if flag:
+        K.append(K[-1])
+    else:
+        K.append(J/k)
+        delta_J = 3*J**2/(1 - (J/(4*0.7075*k))**(2))
+        k *= b
+        J += delta_J
 
 plt.plot(np.arange(100,10,-1), K)
 plt.show()
