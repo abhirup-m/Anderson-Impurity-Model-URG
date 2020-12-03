db712b92c9249c3fe0e5c444234cd76207c7d45a
SearyBlue <mukherjeeabhirup44@gmail.com>
2020-12-03 23:20:51 +0530

    program to solve symmetrical version and show SC fixed point

---
+++ anderson_symmetrical.py
@@ -0,0 +1,31 @@
+#!/bin/python
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+import os
+
+colors = ['r', 'b', 'g', 'y']
+mng = plt.get_current_fig_manager()
+mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname(__file__))
+
+for U in range(0,21,2):
+    V = 0.1
+    V2 = 0.1
+    D_o = 100
+    b = 0.9999
+    w = (D_o - U)/4
+    D = D_o
+    col = colors[0]
+    colors = colors[-1:] + colors[0:-1]
+    for i in range(100):
+        den = U**2 - (4*w - 2*D)**2
+        U += 32*U*D*V**2/den
+        V += -D*V*V2/(U + 4*w - 2*D)
+        plt.scatter(U, V, marker='.', color=col)
+        D *= b
+        if den * (U**2 - (4*w - 2*D)**2) <=0 or U <=0:
+            break
+    print (U,V)
+plt.xlabel("U")
+plt.ylabel("V")
+plt.show()
