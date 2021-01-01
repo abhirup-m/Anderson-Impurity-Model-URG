58aeafe2d5e7ae18a6b8ddf6e8159d0864f7f784
SearyBlue <mukherjeeabhirup44@gmail.com>
2021-01-01 11:49:03 +0530

    added rg solving program and output plots

--- anderson_symmetrical.py
+++ anderson_symmetrical.py
@@ -1,14 +1,9 @@
 #!/bin/python
 
-#   import numpy as np
-import matplotlib as mpl
+import matplotlib
 from matplotlib import pyplot as plt
-import os
-
-colors = ['r', 'b', 'g', 'y']
-mng = plt.get_current_fig_manager()
-mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname(__file__))
-
+import numpy as np
+matplotlib.rcParams['text.usetex'] = True
 #    for U in range(10,20,50):
 #        V = 0.1
 #        V2 = 0.1
@@ -48,20 +43,47 @@ mpl.rcParams["savefig.directory"] = os.chdir(os.path.dirname(__file__))
 #        print (D,U)
 #        break
 
+# Constant hyb, vary starting D, plot flow of U for fixed starting U, omega = 0, J = 2
 
-U = 100
-V = 0.01
-V2 = V
-b = 0.9999
-Do = 50
-D = Do
-for i in range(10):
-    denU = D**2/4 - D*U + 15*U**2/16
-    denV = D - 3*U/2
-    print(U, V)
-    U += 2*U*D*V/denU
-    V += -V*V2*D/denV
-    D *= b
+def fixed_J():
+    for D0 in np.arange(0.1,21,5):
+        J = 0.1
+        D = D0
+        U = 10
+        b = 0.99
+        while D > 0:
+            plt.scatter(D,U)
+            deltaU = J**2 * D**(5/2) * (6*D + J)/(4 * (D**2 - J**2/16))
+            U += deltaU
+            D *= b
+            if deltaU * (D**2 - J**2/16) <= 0:
+                print (D,U)
+                break
+    plt.title(r'J fixed $\rightarrow \omega=0, J=2, U_0=10, D_0 \in [1,20]$')
+    plt.xlabel(r'D')
+    plt.ylabel(r'U')
+    plt.show()
 
-plt.plot([1, 2, 3], [1, 4, 9])
-plt.show()
+
+def all_flow():
+    J = 0.1
+    D = 20
+    U = 100
+    b = 0.99
+    while D > 0:
+        print (D**2 - J**2/16)
+        plt.scatter(J,U)
+        deltaU = J**2 * D**(5/2) * (6*D + J)/(4 * (D**2 - J**2/16))
+        deltaJ = J**2 * D**2 / (2 * (D**2 - J**2/16))
+        U += deltaU
+        J += deltaJ
+        D *= b
+        if deltaU * (D**2 - J**2/16) <= 0:
+            print (J,U)
+            break
+    plt.xlabel(r'J')
+    plt.ylabel(r'U')
+    plt.title(r'both flow $\rightarrow \omega=0, J_0=0.1, U_0=100, D_0 = 20$')
+    plt.show()
+
+fixed_J()
