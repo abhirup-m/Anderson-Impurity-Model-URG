bc49b984b1622d1e4139212015ef87e560040771
SearyBlue <mukherjeeabhirup44@gmail.com>
2021-02-15 00:47:32 +0530

    added new RG equations to program and made it more modular

--- rg_plots/siam-urg.py
+++ rg_plots/siam-urg.py
@@ -1,5 +1,8 @@
 #!/bin/python
 
+
+import itertools
+from math import sqrt
 import matplotlib
 from matplotlib import pyplot as plt
 import numpy as np
@@ -9,95 +12,64 @@ font = {'family' : 'normal',
 
 matplotlib.rc('font', **font)
 matplotlib.rcParams['text.usetex'] = True
-#    for U in range(10,20,50):
-#        V = 0.1
-#        V2 = 0.1
-#        D_o = 100000
-#        b = 0.999999999999
-#    for w in np.linspace(-300,300,20):
-#        U = 1000
-#        V = 0.1
-#        V2 = 0.1
-#        b = 0.999999999999
-#        D = 10
-#        col = colors[0]
-#        colors = colors[-1:] + colors[0:-1]
-#        for i in range(100):
-#            den = U**2 - (4*w - 2*D)**2
-#            U += 32*U*D*V**2/den
-#            V += -D*V*V2/(U + 4*w - 2*D)
-#            plt.scatter(V, U, marker='.', color=col)
-#            D *= b
-#            if den * (U**2 - (4*w - 2*D)**2) <=0 or U <=0:
-#                break
-#        print (w)
-#        plt.show()
-#        plt.clf()
-
-#   U = 1000
-#   V2 = 0.01
-#   b = 0.9999
-#   Do = 100
-#   D = Do
-# for i in range(10000):
-#    den = D**2 - U**2/4
-#    print (den)
-#    U += 2*U*D*V2*Do/den
-#    D *= b
-#    if den * (D**2 - U**2/4) <=0:
-#        print (D,U)
-#        break
 
 
-# Constant hyb, vary starting D, plot flow of U for fixed starting U, omega = 0, J = 2
-def fixed_VJ():
-    for D0 in np.arange(0.1,31,5):
-        D = D0
-        V = 2*D
-        J = V**2 / D
-        U = 10
-        b = 0.99
-        while D > 0:
-            plt.scatter(D,U)
-            deltaU = J**2 * D**(5/2) * (6*D + J)/(4 * (D**2 - J**2/16)) - 4 * V**2 * D * (U + J/4)/((D - U/2 - J/4) * (D + U/2))
-            U += deltaU
-            D *= b
-            if deltaU * (D**2 - J**2/16) <= 0:
-                print (D,U)
-                break
-    plt.title(r'J fixed $\rightarrow \omega=0, J=2, U_0=10, D_0 \in [1,20]$')
-    plt.xlabel(r'D')
-    plt.ylabel(r'U')
-    plt.show()
-
-# Constant hyb, vary starting D, plot flow of U for fixed starting U, omega = 0, J = 2
-def fixed_J():
-    for D0 in np.arange(0.1,21,5):
-        J = 0.1
-        D = D0
-        U = 10
-        b = 0.99
-        while D > 0:
-            plt.scatter(D,U)
-            deltaU = J**2 * D**(5/2) * (6*D + J)/(4 * (D**2 - J**2/16))
-            U += deltaU
-            D *= b
-            if deltaU * (D**2 - J**2/16) <= 0:
-                print (D,U)
-                break
-    plt.title(r'J fixed $\rightarrow \omega=0, J=2, U_0=10, D_0 \in [1,20]$')
-    plt.xlabel(r'D')
-    plt.ylabel(r'U')
-    plt.show()
+def den(w ,D, U, V, J):
+    d1 = w - 0.5 * D + U/2 + J/4
+    d2 = w - 0.5 * D - U/2
+    d3 = w - 0.5 * D + U/2 - J/4
+    return d1, d2, d3
 
 
-# plot flow of U and J for omega = 0
-def all_flow():
+def rg(w, D, U, V, J, flags):
     b = 0.9999
-    colors = ['r','b','y','g']
-    ci=0
-    x = []
-    y = []
+    d = den(w ,D, U, V, J)
+    deltaU = 4 * V**2 * sqrt(D) * (flags[0]/d[0] - flags[1]/d[1]) - J**2 * D * (1/8) * (5*flags[0]/d[0] + flags[2]/d[2])
+    deltaV = (-3/4) * J * sqrt(D) * V * flags[0]/d[0]
+    deltaJ = -J**2 * sqrt(D) * flags[0]/d[0]
+    print (deltaU)
+    U += deltaU
+    V += deltaV
+    J += deltaJ
+    D *= b
+    flags = check_den(w, D, U, V, J, d, flags)
+    return D, U, V, J, flags
+
+def check_den(w, D, U, V, J, d, flags):
+    d_old = d
+    d_new = den(w, D, U, V, J)
+    #print (d_new)
+    for i in range(len(d_old)):
+        if d_old[i] * d_new[i] <= 0 or d_new[i] > 10**5:
+            flags[i] = 0
+    return flags
+
+
+def init_check_den(w, D, U, V, J):
+    d = den(w, D, U, V, J)
+    flags = [1 if di != 0 else 0 for di in d]
+    return flags
+
+
+def all_flow():
+    U_arr = V_arr = J_arr = []
+    w_0 = [0]
+    D_0 = [1]
+    V_0 = [1]
+    J_0 = [2]
+    U_0 = [10]
+    for w, D, U, V, J in itertools.product(w_0, D_0, U_0, V_0, J_0):
+        print ("Start: w={}, D={}, U={}, V={}, J={}".format(w, D, U, V, J))
+        flags = init_check_den(w, D, U, V, J)
+        while D > 0:
+            if not 1 in flags:
+                break
+            D, U, V, J, flags = rg(w ,D, U, V, J, flags)
+            #print (U)
+        print ("End: U={}, V={}, J={}".format(U, V, J))
+
+"""
+
     for D0 in [1,10,100,1000]:
         for J0 in [0]:
             D = D0
@@ -154,37 +126,5 @@ def all_flow():
     plt.title(r'Dependence of $U^*$ on $D$($V=0.1$)')
     plt.show()
 
-def func():
-        J = 0.1
-        sign1 = D + U/4 - J/8
-        sign2 = D - U/4 - J/4
-        sign5 = U + J/4
-        count = 0
-        #print ("start val: D={}, J={}, U={}".format(D, J,U))
-        while D > 0:
-            if sign1 * (D + U/4 - J/8) <= 0:
-                print ("cut by 1,after",count," steps")
-                break
-            elif sign2 * (D - U/4 - J/4) <= 0:
-                print ("cut by 2,after",count," steps")
-                break
-            elif sign5 * U <= 0 and J0 == 0:
-                U = 0
-                print ("cut by 5,after",count," steps")
-                break
-            sign1 = D + U/4 - J/8
-            sign2 = D - U/4 - J/4
-            sign5 = U + J/4
-            deltaU = 0.5 * V**2 * D**(1/2) * sign5 / (sign1 * sign2)
-            deltaJ = J**2 * D**(1/2) / (8 * sign1)
-            deltaV = V * D**(1/2) * (3*J/4) / sign1
-            U += deltaU
-            J += deltaJ
-            V += deltaV
-            D *= b
-            count += 1
-        print ("End val: D={}, U={}, J={}".format(D,U,J))
-        if U < 0:
-            input()
-
+"""
 all_flow()
