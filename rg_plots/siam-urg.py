30c278ee3ea656dfce09d7a9153513c43d9174b3
Abhirup <mukherjeeabhirup44@gmail.com>
2021-03-09 10:58:55 +0530

    added J vs D plot with w = -D/2 and log scales on both axes

--- rg_plots/siam-urg.py
+++ rg_plots/siam-urg.py
@@ -91,22 +91,22 @@ def check_fp(w, D, U, V, J, d, flags, deltas):
 
 def all_flow():
     '''master function to call other functions'''
-    for Dmax in [10]:
-        for w in [-0.1]:
+    plt.title(r'Bare values: $V=1, J=D/5, \epsilon_d=4, \omega=-D/2$')
+    for Dmax in [1, 10,100,1000,10000,100000]:
+        for w in [-Dmax/2]:
             N = 10 * Dmax
             V = 1
-            J = 0.1
+            J = Dmax/5
             ed = 4
-            plt.title(r'Bare values: $V={}, J={}, \epsilon_d={}, \omega={}, D = {}$'.format(V, J, ed, w, Dmax))
             old_den = den(w, Dmax, ed, J)[3]
             flag = False
-            X = []
-            Y = []
-            Z = []
+            #X = []
+            #Y = []
+            #Z = []
             for D in np.linspace(Dmax, 0, N):
-                X.append(D)
-                Y.append(np.log10(J))
-                Z.append(ed)
+                #X.append(D)
+                #Y.append(np.log10(J))
+                #Z.append(ed)
                 new_den = den(w, D, ed, J)[3]
                 if old_den * new_den <= 0: 
                     flag = True
@@ -115,19 +115,19 @@ def all_flow():
                     old_den = new_den
 
                 ed, V, J = rg(w, D, ed, V, J)
-                print ("ed is",ed)
 
             if flag is True: 
-               print ("End: Dmax={}, D*={}, J*={}".format(Dmax, D, J))
-               plt.plot(X, Y, label=r'$J$')
-               plt.scatter(X[0], Y[0], color="g", label="start")
-               plt.scatter(X[-1], Y[-1], color="r", label="end")
-               plt.plot(X, Z, label=r'$\epsilon_d$')
-               plt.scatter(X[0], Z[0], color="g")
-               plt.scatter(X[-1], Z[-1], color="r")
-               plt.legend()
-               plt.xlabel(r'$D$')
-               plt.ylabel(r'$\epsilon_d, \log_{10}J$')
+                print ("End: Dmax={}, D*={}, J*={}".format(Dmax, D, J))
+                plt.scatter(np.log10(Dmax), np.log10(J))
+                #plt.plot(X, Y, label=r'$J$')
+                #plt.scatter(X[0], Y[0], color="g", label="start")
+                #plt.scatter(X[-1], Y[-1], color="r", label="end")
+                #plt.plot(X, Z, label=r'$\epsilon_d$')
+                #plt.scatter(X[0], Z[0], color="g")
+                #plt.scatter(X[-1], Z[-1], color="r")
+                #plt.legend()
+                plt.xlabel(r'$\log_{10}D$')
+                plt.ylabel(r'$\log_{10}J$')
 
     plt.show()
 
