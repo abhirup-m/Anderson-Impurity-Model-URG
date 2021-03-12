d6051b62ce99fd2ec664ac66e4074c6007a68d9a
Abhirup <mukherjeeabhirup44@gmail.com>
2021-03-12 19:55:55 +0530

    fleshed out the omega prescription in kondo, added one more plot and corrected a typo

--- kondo_num/kondo_num.py
+++ kondo_num/kondo_num.py
@@ -25,28 +25,31 @@ def rg2(w,D,J):
     else:
         return J+delta
 
-J = -0.1
-flag = False
-D0 = 10
-plt.title(r'bare values: $\epsilon_q = \epsilon = {}, J = {}$'.format(D0, J))
-N = 1000
-X = [N]
-Y = [J]
-for D in np.linspace(D0,0,N):
-    N -= 1
-    den = D - J/2
-    J = rg1(D, J)
-    if J == 0 or den * (D - J/2) <= 0:
-        flag = True
-        break
-    X.append(N)
+plt.title(r'Bare: $J = -D/10, N = D \times 10$')
+X = []
+Y = []
+for Dmax in [10,100,1000,10000,100000, 1000000]:
+    print (Dmax)
+    J = Dmax/10
+    N = Dmax*10
+    for D in np.linspace(Dmax,0,N):
+        den = D - J/2
+        J = rg1(D, J)
+        if J == 0 or den * (D - J/2) <= 0:
+            break
+    X.append(Dmax)
     Y.append(J)
-if flag:
-    plt.plot(X, Y)
-    plt.xlabel(r'RG step')
-    plt.ylabel(r'$J$')
-    plt.scatter(X[0], Y[0], color='r', marker='o', label="start point")
-    plt.scatter(X[-1], Y[-1], color='g', marker='o', label="fixed point")
-    plt.legend()
-    plt.show()
+    
+plt.scatter(np.log10(X), np.log10(Y), label="data")
+plt.xlabel(r'$\log_{10}D$')
+plt.ylabel(r'$\log_{10}J^*$')
+#plt.scatter(X[0], Y[0], color='r', marker='o', label="start point")
+#plt.scatter(X[-1], Y[-1], color='g', marker='o', label="fixed point")
+#plt.legend()
+linear_model=np.polyfit(np.log10(X),np.log10(Y),1)
+linear_model_fn=np.poly1d(linear_model)
+x_s=np.arange(1,7,1)
+plt.plot(x_s,linear_model_fn(x_s),label="best fit", color="green")
+plt.legend()
+plt.show()
     #plt.savefig('match2.png',bbox_inches='tight', transparent="True", pad_inches=0)
