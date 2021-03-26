ad766de2def1c7f07ccdb4637096c2a01c99efb4
Abhirup <mukherjeeabhirup44@gmail.com>
2021-03-26 19:04:35 +0530

    rewrote RG equations keeping democratic contributions of Kz and Jz, also wrote program for charge+spin

--- spin-charge-symmetrical/spin-charge-symm.py
+++ spin-charge-symmetrical/spin-charge-symm.py
@@ -15,67 +15,80 @@ matplotlib.rc('font', **font)
 matplotlib.rcParams['text.usetex'] = True
 
 
-def den(w, D, U, J):
+def den(w, D, U, J, K):
     ''' Defines and evaluates all the
     denominators in the problem.'''
 
-    d1 = w - 0.5 * D - U/2 + J/4
+    d1 = w - 0.5 * D - U/2 + K/2
     d2 = w - 0.5 * D + U/2 + J/2
-    d3 = w - 0.5 * D + J/4
+    d3 = w - 0.5 * D + J/4 + K/4
 
     return d1, d2, d3
 
 
-def rg(w, D, U, V, J):
+def rg(w, D, U, V, J, K):
     '''Evaluates the change in each coupling 
     at a particular RG step.'''
 
 
-    dens = den(w, D, U, J)
-    deltaU = -4 * V**2 * (1/dens[0] - 1/dens[1]) - (3 * J**2 / 4) * D / dens[2]
-    deltaV = -(3/4) * J * V * (1/dens[1] + 1/dens[2])
-    deltaJ = - 2 * J**2 / dens[2]
+    dens = den(w, D, U, J, K)
+    deltaU = -4 * V**2 * (1/dens[0] - 1/dens[1]) - (3* (J**2 - K**2)/8) * D / dens[2]
+    deltaV = (1/16) * K * V * (1/dens[0] - 1/dens[2]) - (3/4) * J * V * (1/dens[1] + 1/dens[2])
+    deltaJ = - J**2 / dens[2]
+    deltaK = - K**2 / dens[2]
 
     U = 0 if (U + deltaU) * U <= 0 else U + deltaU
     V = 0 if (V + deltaV) * V <= 0 else V + deltaV
     J = 0 if (J + deltaJ) * J <= 0 else J + deltaJ
+    K = 0 if (K + deltaK) * K <= 0 else K + deltaK
 
-    return U, V, J
+    return U, V, J, K
+
+def plot_all(X, Y, Y2, Z, W):
+    '''plots U, J, K, V in separate plots'''
+    plt.plot(X, Y)
+    plt.ylabel("J")
+    plt.show()
+    plt.plot(X, Y2)
+    plt.ylabel("K")
+    plt.show()
+    plt.plot(X, Z)
+    plt.ylabel("U")
+    plt.show()
 
 
 def all_flow():
     '''master function to call other functions'''
-    #fig, ax = plt.subplots(2)
-    for w in np.arange(-1,1,0.1):
-        for U0 in [0.1]:
+    for w in np.arange(-1,0,0.2):
+        for U0 in [5]:
             Dmax = 10
             N = 100
-            V = 0
-            J = 0.1
+            V = 1
+            J = 0.03
+            K = 0.01
             U = U0
             #plt.title(r'$D = {}, V = {}, J = {}, \epsilon_d = {}, \omega = {}$'.format(Dmax, V, J, ed, w))
-            old_den = den(w, Dmax, U, J)[2]
+            old_den = den(w, Dmax, U, J, K)[2]
             X = []
             Y = []
+            Y2 = []
             Z = []
             W = []
             step = N
             for D in np.linspace(Dmax, 0, N):
                 X.append(step)
                 Y.append(J)
+                Y2.append(K)
                 Z.append(U)
                 W.append(V)
-                new_den = den(w, D, U, J)[2]
+                new_den = den(w, D, U, J, K)[2]
                 #print (np.round(old_den,4), np.round(new_den,4))
-                if old_den * new_den <= 0:
-                    print (step, U, J)
-                    plt.plot(X, Y)
-                    plt.show()
-                    plt.plot(X, Z)
-                    plt.show()
+                if old_den * new_den <= 0 and U == 0 and J > 0.03:
+                    print (w, U, J)
+                    plot_all(X, Y, Y2, Z, W)
                     break
                 old_den = new_den
-                U, V, J = rg(w, D, U, V, J)
+                U, V, J, K = rg(w, D, U, V, J, K)
                 step -= 1
 
             #ax.plot(X, Z, marker=".")
