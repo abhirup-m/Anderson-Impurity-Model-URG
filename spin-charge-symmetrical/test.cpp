f2b091be6f1915339b13178250bff9453e3263c1
SearyBlue <mukherjeeabhirup44@gmail.com>
2021-05-30 18:24:36 +0530

    seems like  5 ioms on either side is the limit for reverse RG. Implemented that and computed some entanglement measures

---
+++ spin-charge-symmetrical/test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+using std::setw;
+ 
+double [] main(double x[], double[])
+{
+
+}
+int main () 
+{
+   double bit[2] = {1, 0};
+   int N = 30
+   int dim = 2^N
+   double state[dim];
+   for (int i = 0; i < dim; i++ )
+
+   // std::cout << "Hello, World\n";
+}
